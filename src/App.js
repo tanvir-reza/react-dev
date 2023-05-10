3287840e85b4ba2275c33d076a2f63d88af1753f
Md Tanvir Reza <tanvir.reza.abc@gmail.com>
2023-05-10 23:34:40 +0600

    functional Component

--- src/App.js
+++ src/App.js
@@ -4,22 +4,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+      <App2 />
     </div>
   );
+}
+
+function App2() {
+  return (
+    <div className='App'>
+      <h1>Hello 2</h1>
+    </div>
+  )
 }
 
 export default App;
