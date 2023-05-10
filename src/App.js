8551c4980068e3eafcc29dae4805b9a3cbe34176
Md Tanvir Reza <tanvir.reza.abc@gmail.com>
2023-05-10 23:57:44 +0600

    class props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 
 
-import App2 from './components/Person';
+import Person from './components/Person';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
-      <App2 name="Tanvir" age = "30" >Info1</App2>
-      <App2 name="Reza" age = "30" > Info2</App2>
+      <Person name="Tanvir" age = "30" >Info1</Person>
+      <Person name="Reza" age = "30" > Info2</Person>
     </div>
   );
 }
--- src/components/Person.js
+++ src/components/Person.js
@@ -1,12 +1,32 @@
-function App2(props) {
-  return (
-    <div className='App'>
-          <h1>Name: {props.name}</h1>
-          <h4>Age : {props.age}</h4>
-          <p>Info : { props.children}</p>
-    </div>
-  )
+/* eslint-disable no-useless-constructor */
+import React,{Component} from "react"
+
+
+// function Person(props) {
+//   return (
+//     <div className='App'>
+//           <h1>Name: {props.name}</h1>
+//           <h4>Age : {props.age}</h4>
+//           <p>Info : { props.children}</p>
+//     </div>
+//   )
+// }
+
+
+class Person extends Component{
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div className='App'>
+            <h1>Name: {this.props.name}</h1>
+            <h4>Age : {this.props.age}</h4>
+            <p>Info : { this.props.children}</p>
+           </div>
+        )
+    }
 }
 
 
-export default App2
+export default Person
